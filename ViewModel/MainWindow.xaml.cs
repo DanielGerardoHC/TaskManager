61e7f02588c29922305edde3e0bdab2d9d035b71
Daniel Hernandez <gerardo.hernandez23@itca.edu.sv>
2024-03-09 10:01:04 -0600

    New desing for menu buttons

--- ViewModel/MainWindow.xaml.cs
+++ ViewModel/MainWindow.xaml.cs
@@ -44,5 +44,19 @@ namespace AdministradorDeTareas
         {
             this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+        private void btnMaximize_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+                this.WindowState = WindowState.Maximized;
+            else this.WindowState = WindowState.Normal;
+        }
     }
 }
