dfc6d8d199d0efc04840ed0d11f8ee5d522de322
Daniel Hernandez <gerardo.hernandez23@itca.edu.sv>
2024-03-14 03:12:51 -0600

    New desing of the TaskManagment

--- ViewModel/TaskManagmentModel.cs
+++ ViewModel/TaskManagmentModel.cs
@@ -1,14 +1,60 @@
-﻿using AdministradorDeTareas.ViewModel;
+﻿using AdministradorDeTareas.Model;
+using AdministradorDeTareas.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace AdministradorDeTareas.ViewModel
 {
-    public class TaskManagmentModel : ViewModelBase
+    public class TaskManagmentModel : ViewModelBase, INotifyPropertyChanged
     {
-    }
+        private int _pendingTasks;
+        public int PendingTasks{
+            get => _pendingTasks;
+            set{
+                _pendingTasks = value;
+                OnPropertyChanged(nameof(PendingTasks));
+            }
+        }
+        private int _mediumPriority;
+        public int MediumPriority
+        {
+            get => _mediumPriority;
+            set
+            {
+                _mediumPriority = value;
+                OnPropertyChanged(nameof(MediumPriority));
+            }
+        }
+        private int _highPriority;
+        public int HighPriority
+        {
+            get => _highPriority;
+            set
+            {
+                _highPriority = value;
+                OnPropertyChanged(nameof(HighPriority));
+            }
+        }
+        public TaskManagmentModel()
+        {
+            this.PendingTasks = 10;
+            this.MediumPriority = 8;
+            this.HighPriority = 12;
+        }
+        // event to INotifyPropetyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }  
 }
--- ViewModel/ViewOptionsModel.cs
+++ ViewModel/ViewOptionsModel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace AdministradorDeTareas.ViewModel
 {
@@ -10,4 +12,5 @@ namespace AdministradorDeTareas.ViewModel
     {
 
     }
+
 }
